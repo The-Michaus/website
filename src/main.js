02b3d6d725755d6b158c3a0741ccc25a69b5f0a7
Marcel Michau <7761114+MarcelMichau@users.noreply.github.com>
2026-03-03 17:56:17 +0200

    Refactor: update navbar functionality and styles for improved usability and integration with popover API

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { initIcons } from './modules/icons.js';
-import { initNavbar } from './modules/navbar.js';
 import { initScrollspy } from './modules/scrollspy.js';
 
 initIcons();
 
 window.addEventListener('DOMContentLoaded', () => {
-  initNavbar();
   initScrollspy();
 });
